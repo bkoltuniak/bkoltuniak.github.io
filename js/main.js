9da25be0f5c9a230025c5e3175f521d8e4b5e892
bartoszkoltuniak <koltuniak.bartosz@gmail.com>
2021-03-28 18:49:22 +0200

    add header website

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.dupa-example').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    centerMode: true,
+    variableWidth: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+// $('img').click(function() {
+//     var src = $(this).attr('src');
+//     window.open(src);ł
+//   });
